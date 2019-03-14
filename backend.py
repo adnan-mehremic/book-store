ec2a9c61695528ff3e345e969ea92897f427e57a
mehremic1 <mehremic.a@gmail.com>
2019-03-14 13:19:07 +0100

    Add backend script for our GUI app

---
+++ backend.py
@@ -0,0 +1,57 @@
+import sqlite3
+
+
+def connect():
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS book "
+                "(id INTEGER PRIMARY KEY, title text, author text, year integer, publication text)")
+    conn.commit()
+    conn.close()
+
+
+def insert(title, author, year, publication):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("INSERT INTO book VALUES (NULL,?,?,?,?)", (title, author, year, publication))
+    conn.commit()
+    conn.close()
+
+
+def view():
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM book")
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+
+def search(title="", author="", year="", publication=""):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR publication=?",
+                (title, author, year, publication))
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+
+def delete(id):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("DELETE FROM book WHERE id=?", (id,))
+    conn.commit()
+    conn.close()
+
+
+def update(id, title, author, year, publication):
+    conn = sqlite3.connect("books.db")
+    cur = conn.cursor()
+    cur.execute("UPDATE book SET title=?, author=?, year=?, publication=? WHERE id=?",
+                (title, author, year, publication, id))
+    conn.commit()
+    conn.close()
+
+
+connect()
